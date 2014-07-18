f08f929f3fdf13a6924a824c7da13ef34a70d88e
Graham Odds <godds@scottlogic.co.uk>
2014-07-18 15:50:12 +0100

    moved build to grunt

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt) {
+    
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-modernizr');
+    
+    var taskConfig = {
+        
+        pkg: grunt.file.readJSON("package.json"),
+                
+        less: {
+            build: {
+                files: {
+                    "style.css": "style.less"
+                }
+            }
+        },
+        
+        autoprefixer: {
+            build: {
+                files: {
+                    "style.css": "style.css"
+                }
+            }
+        },
+        
+        browserify: {
+            build: {
+                "script.js": [ "scripts/**/*.js" ]
+            }
+        },
+        
+        uglify: {
+            build: {
+                files: {
+                    "script.min.js": [ "script.js" ]
+                }
+            }
+        },
+        
+        modernizr: {
+            build: {
+                devFile: "modernizr-dev.js",
+                outputFile: "modernizr-custom.min.js"
+            }
+        }
+        
+    };
+    
+    grunt.initConfig(taskConfig);
+    
+    grunt.registerTask("dev", [ "less", "autoprefixer", "browserify", "modernizr" ]);
+    grunt.registerTask("build", [ "less", "autoprefixer", "browserify", "uglify", "modernizr" ]);
+    
+};
