f6bf39aa1b2ba68d32981c42c0b62bafe923844b
Chris Grant <cgrant@scottlogic.co.uk>
2014-09-08 16:59:34 +0100

    Removing unnecessary code.

--- scripts/viewModel/themeViewModel.js
+++ scripts/viewModel/themeViewModel.js
@@ -1,13 +1,3 @@
-/**
- * ThemeViewModel
- * is responsible for linking the Parse.Object that store the theme identifying data (associated with the user)
- * with the Parse-stored json data that?s mapped to and from the ThemeModel owned by the ApplicationViewModel
- * (the "current theme") to drive the editor and previews.
- * It is associated with a themeManager, which it delegates most of it's methods to.
- *
- * TODO Introducing ThemeStyles as the underlying associated model.
- */
-
 var ThemeStyles = require('../model/themeStyles'),
     configMapping = require('./configMapping'),
     ErrorReportingService = require('../service/errorReportingService'),
